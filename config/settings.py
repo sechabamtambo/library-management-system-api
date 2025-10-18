e9b5af05b221b91f9526d386aeec69a6c5ee2e37
Sechaba Mtambo <mtambosechaba@gmail.com>
2025-10-18 10:32:12 +0200

    added the api in my path

--- config/settings.py
+++ config/settings.py
@@ -127,9 +127,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',  
-        'rest_framework.authentication.SessionAuthentication', 
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+        
     ]
 }
